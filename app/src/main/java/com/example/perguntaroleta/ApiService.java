bbf04b9b30f3fb05e6da438e73b46dd7fef55ec8
Izaque Rolim <izaque.rolim.canavarro@gmail.com>
2023-05-13 20:13:30 -0400

    create repo

---
+++ app/src/main/java/com/example/perguntaroleta/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.perguntaroleta;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiService {
+    @GET("Casa_{number}/Pergunta.json")
+    Call<Pergunta> getQuestion(@Path("number") int number);
+}
